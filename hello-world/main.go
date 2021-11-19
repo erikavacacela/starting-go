45fb4058dc7cac3a22c85c453936443089ea4538
Erika Vacacela <erikavacacelaeb@gmail.com>
2021-11-19 18:08:29 -0500

    Added main classes

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
+	"os"
+	"strings"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
+	}
+}
+
+/*func main() {
 	// var whatToSay string
 	// whatToSay = "Hello Wold!"
 
@@ -20,4 +46,4 @@ func main() {
 
 func sayHelloWorld(whatToSay string) {
 	fmt.Println(whatToSay)
-}
+}*/
